bbbe2cca4e7cfb148c050a920f44f78a219d0253
ImranManihar <imranmanihar77@gmail.com>
2023-10-22 12:01:30 +0530

    Initial commit

---
+++ src/Process.java
@@ -0,0 +1,12 @@
+import java.util.Date;
+
+public class Process {
+
+	public static void main(String[] args) {
+		System.out.println("hello");
+		System.out.println("java programmer");
+		System.out.println(new Date());
+
+	}
+
+}
